b6327ea72f7e9c6c559f602b0a56fbbe1abb2d4f
ThatOneCoderYouLove <132755457+ThatOneCoderYouLove@users.noreply.github.com>
2023-07-17 13:57:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class MainWindow(QMainWindow):
         self.browser.urlChanged.connect(self.update_url)
 
     def navigate_home(self):
-        self.browser.setUrl(QUrl('http://programming-hero.com'))
+        self.browser.setUrl(QUrl('https://webhubb.vercel.app/Webhub.html'))
 
     def navigate_to_url(self):
         url = self.url_bar.text()
@@ -50,6 +50,6 @@ class MainWindow(QMainWindow):
 
 
 app = QApplication(sys.argv)
-QApplication.setApplicationName('My Cool Browser')
+QApplication.setApplicationName('Patricks Browser')
 window = MainWindow()
 app.exec_()
